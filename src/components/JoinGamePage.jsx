358def4e2d84b499ff81b572b1f571e3e21a1266
grubor-maja <maja.grubor@outlook.com>
2023-12-18 11:48:26 +0100

    Initialized Kahoot app

---
+++ src/components/JoinGamePage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+function JoinGamePage() {
+    return (
+        <div>
+            <div className="joinGameContainer">
+            <h1>Join game</h1>
+            </div>
+            
+        </div>
+      );
+}
+
+export default JoinGamePage;
